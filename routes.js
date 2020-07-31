df0213651d09c8c6d750ee13aaf8229a8b1cee36
AbisaiSan <abisaiweb7@gmail.com>
2020-07-31 12:24:43 -0300

    first commit

---
+++ routes.js
@@ -0,0 +1,22 @@
+const express = require('express')
+//Variavel responsavel pelas rotas
+const routes = express.Router()
+
+
+
+routes.get('/', function(req, res){
+  return res.redirect("/teachers")
+})
+
+routes.get('/teachers', function(req, res){
+  return res.send("teachers")
+})
+
+routes.get('/students', function(req, res){
+  return res.send("Students")
+})
+
+
+//Exportando o arquivo
+module.exports = routes
+
